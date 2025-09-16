c8c0dcd64fcd11013c6fcf95221f7fe5b60328f5
olgo1 <mnggrnnk@gmail.com>
2025-09-16 17:03:51 +0300

    Update manifest.js

--- tasks/manifest.js
+++ tasks/manifest.js
@@ -2,7 +2,7 @@
 // Add the path to any new task file you create.
 
 window.taskFiles = [
-    'tasks/time_tasks.js',
+    'tasks/4_time_tasks.js',
     'tasks/num_tasks.js',
     'tasks/4_count_tasks.js',
     // e.g., 'tasks/fractions_tasks.js',
