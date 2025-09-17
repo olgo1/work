7cc430aafd6dc54a6f48cd834bc3f32a4a3b24cc
olgo1 <mnggrnnk@gmail.com>
2025-09-17 13:48:47 +0300

    Update 4_count_3_sheet.js

--- sheets/4_count_3_sheet.js
+++ sheets/4_count_3_sheet.js
@@ -13,7 +13,7 @@ const problemSelectors = [
         filter: {
             // Указываем список номеров. Задача будет выбрана, 
             // если её номер - один из этих трёх.
-            number: ["4_count_13", "4_count_14", "4_count_15", "4_count_16", "4_count_17", "4_count_18", "4_count_19", "4_count_20", "4_count_21", "4_count_22", "4_count_23", , "4_count_24", , "4_count_25", , "4_count_26"]
+            number: ["4_count_13", "4_count_14", "4_count_15", "4_count_16", "4_count_17", "4_count_18", "4_count_19", "4_count_20", "4_count_21", "4_count_22", "4_count_23", , "4_count_24", "4_count_25", "4_count_26"]
         }
     }
 ];
