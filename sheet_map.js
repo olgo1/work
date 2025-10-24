d2a68b334a692e48d1c5287b88672bf67f1b58cd
olgo1 <mnggrnnk@gmail.com>
2025-10-24 12:13:51 +0300

    Fix netlify proxy rules for assets

--- sheet_map.js
+++ sheet_map.js
@@ -1,7 +1,7 @@
 // sheet_map.js
 window.sheetMap = {
   '76hoEqJ8OS': '4count0_sheet',
-  'LHQdJdPZZo': '4count1_sheet'
-  'lCfp1amSuy': '4count2_sheet'
+  'LHQdJdPZZo': '4count1_sheet',
+  'lCfp1amSuy': '4count2_sheet',
   '6IQNhOaRm4': '4count3_sheet'
 };
